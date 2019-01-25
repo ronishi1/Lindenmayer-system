25c9445375b591ca3cca10abf8174d3246b9e72c
Ray <rayonishi11@gmail.com>
2019-01-25 16:21:17 -0500

    Binary fractal tree done

--- sketch.js
+++ sketch.js
@@ -1,39 +1,47 @@
 // L-system rules
 var axiom = "0";
 var sent = axiom;
-var n = 3;
+var n = 8;
 
 // Rule for fractal binary tree
 var rules = [];
 rules[0] = {
-  letter:"1",
+  char:"1",
   sub:"11"
 }
 rules[1] = {
-  letter:"0",
+  char:"0",
   sub:"1[0]0"
 }
+
+var index = 0;
+var len = 4;
 
 // *****************************************
 // Code for generating L system sentence
 function setup(){
-  createCanvas(500, 500);
-  x = width / 2;
-  y = height - 1;
+  createCanvas(1000, 1000);
   background(105);
+  angleMode(RADIANS);
   for(var i = 0; i < n; i++){
     sent = Lgen(sent);
   }
+  visualizeBinary();
 }
 
 function Lgen(sentence){
   var next = "";
   for(var i = 0; i < sentence.length; i++){
     var cur = sentence.charAt(i);
+    var inrule = false;;
     for(var j = 0; j < rules.length; j++){
-      if(cur == rules[j].letter){
+      if(cur == rules[j].char){
         next += rules[j].sub;
+        inrule=true;
       }
+    }
+    if(!inrule){
+      next += cur;
     }
   }
   console.log(next);
@@ -41,3 +49,26 @@ function Lgen(sentence){
 }
 
 // ***********************************************
+function visualizeBinary(){
+  stroke(255);
+  resetMatrix();
+  translate(width/2,height-1)
+  for(var i = 0; i < sent.length; i++){
+    var cur = sent.charAt(i);
+    if(cur == "0"){
+      line(0,0,0,-len);
+    }
+    if(cur == "1"){
+      line(0,0,0,-len);
+      translate(0,-len);
+    }
+    if(cur == "["){
+      push();
+      rotate(PI/4);
+    }
+    if(cur == "]"){
+      pop();
+      rotate(-PI/4)
+    }
+  }
+}
