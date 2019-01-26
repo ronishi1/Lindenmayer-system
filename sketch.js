ccb0402615b43018dae5a68f7830f14b76fdac63
Ray <rayonishi11@gmail.com>
2019-01-26 14:09:03 -0500

    Added fractal plant & added old version of p5 due to issue with mousePressed

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-// L-system rules
-var axiom = "0";
-var sent = axiom;
-var n = 8;
+// ******************************
+// Fractal binary tree variables/rules
+var axiom0 = "0";
+var sent0 = axiom0;
+var numRecurs = 8;
+var len = 3;
 
-// Rule for fractal binary tree
 var rules = [];
 rules[0] = {
   char:"1",
@@ -13,23 +14,55 @@ rules[1] = {
   char:"0",
   sub:"1[0]0"
 }
+// ******************************
 
-var index = 0;
-var len = 4;
-
-// *****************************************
-// Code for generating L system sentence
-function setup(){
-  createCanvas(1000, 1000);
-  background(105);
-  angleMode(RADIANS);
-  for(var i = 0; i < n; i++){
-    sent = Lgen(sent);
-  }
-  visualizeBinary();
+// ******************************
+// Fractal plant variable/rules
+var axiom1 = "X";
+var sent1 = axiom1;
+var rules1 = [];
+var len1 = 3;
+var lenholder = len1;
+rules1[0] = {
+  char:"X",
+  sub:"F+[[X]-X]-F[-FX]+X"
 }
 
-function Lgen(sentence){
+rules1[1] = {
+  char:"F",
+  sub:"FF"
+}
+// ******************************
+
+// *****************************************
+function setup(){
+  createCanvas(900, 900);
+  reset();
+  angleMode(RADIANS);
+
+  // Button for generating binary fractal tree
+  var treeButton = createButton("Binary Fractal Tree Gen");
+  treeButton.mousePressed(visualizeBinary);
+
+  // Button for generating fractal plant
+  var plantButton = createButton("Fractal Plant");
+  plantButton.mousePressed(genPlant);
+
+  var resetButton = createButton("Reset");
+  resetButton.mousePressed(reset);
+}
+
+function reset(){
+  sent0 = axiom0;
+  sent1 = axiom1;
+  len1 = lenholder;
+  background(105);
+}
+// *****************************************
+
+// *****************************************
+// Code for binary fractal tree
+function Lgen0(sentence){
   var next = "";
   for(var i = 0; i < sentence.length; i++){
     var cur = sentence.charAt(i);
@@ -48,13 +81,15 @@ function Lgen(sentence){
   return next;
 }
 
-// ***********************************************
 function visualizeBinary(){
+  for(var i = 0; i < numRecurs; i++){
+    sent0 = Lgen0(sent0);
+  }
   stroke(255);
   resetMatrix();
   translate(width/2,height-1)
-  for(var i = 0; i < sent.length; i++){
-    var cur = sent.charAt(i);
+  for(var i = 0; i < sent0.length; i++){
+    var cur = sent0.charAt(i);
     if(cur == "0"){
       line(0,0,0,-len);
     }
@@ -72,3 +107,54 @@ function visualizeBinary(){
     }
   }
 }
+// *****************************************
+function genPlant(){
+  sent1 = Lgen1(sent1);
+  len1 *= .95
+  visualizePlant();
+}
+
+function Lgen1(sentence){
+  var next = "";
+  for(var i = 0; i < sentence.length; i++){
+    var cur = sentence.charAt(i);
+    var inrule = false;;
+    for(var j = 0; j < rules1.length; j++){
+      if(cur == rules1[j].char){
+        next += rules1[j].sub;
+        inrule=true;
+      }
+    }
+    if(!inrule){
+      next += cur;
+    }
+  }
+  console.log(next);
+  return next;
+}
+
+function visualizePlant(){
+  stroke(255);
+  resetMatrix();
+  translate(width/2,height-1)
+  for(var i = 0; i < sent1.length; i++){
+    var cur = sent1.charAt(i);
+    if(cur == "F"){
+      line(0,0,0,-len1);
+      translate(0,-len1);
+    }
+    else if(cur == "+"){
+      rotate(0.436332);
+    }
+    else if(cur == "-"){
+      rotate(-0.436332);
+    }
+    else if(cur == "["){
+      push();
+    }
+    else if(cur == "]"){
+      pop();
+    }
+  }
+}
+// *****************************************
